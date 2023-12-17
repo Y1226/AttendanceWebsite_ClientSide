0b9b41b6eb8bb4299e24a6cbb8818513ed23b939
Michal-Hakim <gh052766@gmail.com>
2023-12-17 15:04:15 +0200

    update css

--- src/Components/WebSetup/AddMajor.jsx
+++ src/Components/WebSetup/AddMajor.jsx
@@ -9,17 +9,19 @@ import { FillStaffData } from "../../Redux/Actions/TableActions/Manager/TeacherT
 import Select from 'react-select'
 
 export const AddMajor = () => {
-
+    debugger
     let majors = useSelector(x => x.ManagerMajorTableReducer.MajorList)
     let staff = useSelector(x => x.TeacherTableReducer.StaffList)
     let navigate = useNavigate()
     let dispatch = useDispatch()
     let other = useRef()
     let [isOtherChecked, setIsOtherChecked] = useState(false)
-    let [number, setNumber] = useState(1)
     const listSelectStaff = []
+    const [majorsToSelect, setMajorsToSelect] = useState([])
 
-    const [animal, setAnimal] = useState(null)
+    // majors.forEach(e => {
+    //     majorsToSelect.push({nameMajor: e.majorName, statusMajor: true })
+    // })
 
     staff.forEach(e => {
         listSelectStaff.push({ code: e.staffCode, value: e.userFirstName + " " + e.userLastName, label: e.userFirstName + " " + e.userLastName })
@@ -35,12 +37,12 @@ export const AddMajor = () => {
         fetchData()
     }, [dispatch])
 
-    const handleChange = (value) => {
-        debugger
-        console.log("value:", value);
-        alert("code: " + value.code + ", value: " + value.value)
-        setAnimal(value);
-    };
+    // const handleChange = (value) => {
+    //     debugger
+    //     console.log("value:", value);
+    //     alert("code: " + value.code + ", value: " + value.value)
+    //     setAnimal(value);
+    // };
 
     // const ShowInputs = () => {
     //     setIsOtherChecked(other.current.checked)
@@ -88,7 +90,7 @@ export const AddMajor = () => {
     //         let br = document.getElementsByTagName('br')
     //         br[br.length - 1].remove()
     //     }
-    const elementsOther = [];
+
     // const createElementsForOther = (number) => {
     //     // var elementsOther = [];
     //     for (let i = 0; i < number; i++) {
@@ -131,11 +133,6 @@ export const AddMajor = () => {
         // ===================================================================
         debugger
         if (other.current.checked) {
-            elementsOther.forEach(e => {
-                debugger
-            })
-        }
-        if (other.current.checked) {
             chosenMajor = document.getElementsByClassName('spaces')
             let chosenMajorCoordinator = document.getElementsByClassName('selectCoordinator')
             majorList = {}
@@ -154,32 +151,131 @@ export const AddMajor = () => {
 
         navigate('../addCourseToMajor')
     }
-    const arrOtherCodesCoordinator = []
+
+    const [selectValues, setSelectValues] = useState([{ selectValue: null, inputValue: '' }]);
+
+    const handleChange = (index, selectedOption) => {
+        const updatedValues = [...selectValues];
+        updatedValues[index] = { ...updatedValues[index], selectValue: selectedOption };
+        setSelectValues(updatedValues);
+    };
+
+    const handleInputChange = (index, inputValue) => {
+        const updatedValues = [...selectValues];
+        updatedValues[index] = { ...updatedValues[index], inputValue };
+        setSelectValues(updatedValues);
+    };
+
+    const handleAddSelect = () => {
+        setSelectValues([...selectValues, { selectValue: null, inputValue: '' }]);
+    };
+
+    const handleDeleteSelect = (index) => {
+        const updatedValues = selectValues.filter((_, i) => i !== index);
+        setSelectValues(updatedValues);
+    };
+
+    const handleSubmit = () => {
+        const dictionary = {};
+        selectValues.forEach((value, index) => {
+            dictionary[`select${index}`] = value.selectValue;
+            dictionary[`input${index}`] = value.inputValue;
+        });
+        console.log(dictionary);
+        console.log(selectValues);
+        SaveMajors();
+    };
 
     return <>
         <div>
-            <div className="title">
+            <div className="titleAddMajor">
                 <label className="label" style={{ fontSize: '22px' }}>:בחר את המסלולים בסמינר</label>
             </div>
             <hr style={{ background: '#607d8b', height: '1px' }} />
             <br />
-            {
-                majors.map((x, i) => <>
-                    <div key={i} className='divCheckbox'>
-                        <input className="checkbox" type="checkbox" />
-                        <label className="label">{x.majorName}</label>
-                    </div>
-                    <br />
-                </>)
-            }
-            {/* <input type="checkbox" id="other" ref={other} onChange={() => { setIsOtherChecked(other.current.checked) }}/><label>אחר</label> <br /> */}
-            {/* <input type="checkbox" id="other" ref={other} onChange={() => ShowInputs()} /><label>אחר</label> <br /> */}
+            {majors.map((value, index) => (
+                <div key={index} className='divCheckbox'>
+                    <input className="checkbox" type="checkbox" />
+                    <label className="label">{value.majorName}</label>
+                </div>
+            ))}
             <div className='divCheckbox'>
-                <input className="checkbox" type="checkbox" ref={other} onChange={() => setIsOtherChecked(other.current.checked)} />
+                <input className="checkbox" type="checkbox" ref={other} onChange={() => { setIsOtherChecked(other.current.checked); if (!isOtherChecked) setSelectValues([{ selectValue: null, inputValue: '' }]) }} />
                 <label className='label'>אחר</label>
             </div>
-            {/*  */}
-            {isOtherChecked && [...Array(number)].map((e, i) => {
+        </div>
+        <div>
+            {isOtherChecked && selectValues.map((value, index) => (
+                <div key={index} className='spaces'>
+                    <div className="col-3 input-effect">
+                        <input
+                            className="effect-19"
+                            type="text"
+                            placeholder="track name"
+                            value={value.inputValue}
+                            onChange={(e) => handleInputChange(index, e.target.value)}
+                            onFocus={(e) => e.target.placeholder = ""}
+                            onBlur={(e) => e.target.placeholder = "track name"}
+                        />
+                        <label>track name</label>
+                        <span className="focus-border">
+                            <i></i>
+                        </span>
+                    </div>
+                    <div className='SelectComponent'>
+                        <Select
+                            placeholder="Select route coordinator"
+                            maxMenuHeight={130}
+                            value={value.selectValue}
+                            onChange={(selectedOption) => handleChange(index, selectedOption)}
+                            options={listSelectStaff}
+                        />
+                    </div>
+                    <button className="plusButton" style={{ flexDirection: 'column' }} onClick={() => handleDeleteSelect(index)}>
+                        <svg className="bin-top" viewBox="0 0 39 7">
+                            <line y1="5" x2="39" y2="5" stroke-width="4"></line>
+                            <line x1="12" y1="1.5" x2="26.0357" y2="1.5" stroke-width="3"></line>
+                        </svg>
+                        <svg className="bin-bottom" viewBox="0 0 33 39">
+                            <mask>
+                                <path d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"></path>
+                            </mask>
+                            <path d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"></path>
+                            <path d="M12 6L12 29" stroke-width="4"></path>
+                            <path d="M21 6V29" stroke-width="4"></path>
+                        </svg>
+                    </button>
+                </div>
+            ))}
+            <br />
+            {isOtherChecked &&
+                <div tabindex="0" className="plusButton plus" onClick={handleAddSelect}>
+                    <svg className="plusIcon" viewBox="0 0 30 30">
+                        <path d="M13.75 23.75V16.25H6.25V13.75H13.75V6.25H16.25V13.75H23.75V16.25H16.25V23.75H13.75Z"></path>
+                    </svg>
+                </div>}
+            <br />
+            <div className="plusButton div_next" style={{ marginBottom: '50px' }} onClick={handleSubmit}>
+                <svg className="arrow" viewBox="0 0 20 20">
+                    <path d="M18.271,9.212H3.615l4.184-4.184c0.306-0.306,0.306-0.801,0-1.107c-0.306-0.306-0.801-0.306-1.107,0
+                            L1.21,9.403C1.194,9.417,1.174,9.421,1.158,9.437c-0.181,0.181-0.242,0.425-0.209,0.66c0.005,0.038,0.012,0.071,0.022,0.109
+                            c0.028,0.098,0.075,0.188,0.142,0.271c0.021,0.026,0.021,0.061,0.045,0.085c0.015,0.016,0.034,0.02,0.05,0.033l5.484,5.483
+                            c0.306,0.307,0.801,0.307,1.107,0c0.306-0.305,0.306-0.801,0-1.105l-4.184-4.185h14.656c0.436,0,0.788-0.353,0.788-0.788
+                            S18.707,9.212,18.271,9.212z">
+                    </path>
+                </svg>
+                <label className="text_next">הבא</label>
+            </div>
+            {/* <button onClick={handleSubmit}>Submit</button> */}
+        </div >
+    </>
+}
+
+/*
+<input type="checkbox" id="other" ref={other} onChange={() => { setIsOtherChecked(other.current.checked) }}/><label>אחר</label> <br />
+<input type="checkbox" id="other" ref={other} onChange={() => ShowInputs()} /><label>אחר</label> <br /> 
+  
+{isOtherChecked && [...Array(number)].map((e, i) => {
                 return <div key={i} className='spaces'>
                     <div className="col-3 input-effect">
                         <input className="effect-19" type="text" placeholder={"שם מסלול"} onFocus={(e) => e.target.placeholder = ""} onBlur={(e) => e.target.placeholder = "שם מסלול"} />
@@ -193,9 +289,8 @@ export const AddMajor = () => {
                     </div>
                 </div>
             })}
-
-            {/* {isOtherChecked && createElementsForOther(number)} */}
-            {isOtherChecked &&
+{isOtherChecked && createElementsForOther(number)}
+{isOtherChecked &&
                 <div className='spaces'>
                     <div tabindex="0" className="plusButton" onClick={() => { setNumber(number + 1); arrOtherCodesCoordinator.push(null) }}>
                         <svg className="plusIcon" viewBox="0 0 30 30">
@@ -209,31 +304,21 @@ export const AddMajor = () => {
                     </div>
                 </div>
             }
-        </div>
-        <div className="plusButton div_next" style={{ marginBottom: '50px' }} onClick={() => SaveMajors()}>
-            <svg className="arrow" viewBox="0 0 20 20">
-                <path d="M18.271,9.212H3.615l4.184-4.184c0.306-0.306,0.306-0.801,0-1.107c-0.306-0.306-0.801-0.306-1.107,0
-                            L1.21,9.403C1.194,9.417,1.174,9.421,1.158,9.437c-0.181,0.181-0.242,0.425-0.209,0.66c0.005,0.038,0.012,0.071,0.022,0.109
-                            c0.028,0.098,0.075,0.188,0.142,0.271c0.021,0.026,0.021,0.061,0.045,0.085c0.015,0.016,0.034,0.02,0.05,0.033l5.484,5.483
-                            c0.306,0.307,0.801,0.307,1.107,0c0.306-0.305,0.306-0.801,0-1.105l-4.184-4.185h14.656c0.436,0,0.788-0.353,0.788-0.788
-                            S18.707,9.212,18.271,9.212z">
-                </path>
-            </svg>
-            <label className="text_next">הבא</label>
-        </div>
-        {/* ****************************************** */}
-        {/* <div id="addedInput" hidden>
+
+
+<div id="addedInput" hidden>
                 <div id="inputPlace"></div>
                 <button style={{ backgroundColor: 'darkslateblue', padding: '5px' }} onClick={() => AddInputs()}>+</button>
                 <button style={{ backgroundColor: 'lightblue', padding: '5px' }} onClick={() => RemoveInput()}>-</button>
-            </div> */}
-        {/* <button style={{ backgroundColor: 'pink', padding: '5px' }} onClick={() => SaveMajors()}>הבא</button> */}
-        {/*  */}
-        {/*{[...Array(number)].map((e, i) => {
+            </div>
+<button style={{ backgroundColor: 'pink', padding: '5px' }} onClick={() => SaveMajors()}>הבא</button>
+{[...Array(number)].map((e, i) => {
             return <li key={i}>{i}</li>
         })}
          <input type="button" value="plus" onClick={()=> {setNumber(number+1)}}/>
         <br />
-        <input type="button" value="minus" onClick={() => { setNumber(number - 1) }} /> */}
-    </>
-}
+        <input type="button" value="minus" onClick={() => { setNumber(number - 1) }} />
+<button onClick={() => handleDeleteSelect(index)}>Delete Select</button>
+<button onClick={handleAddSelect}>Add Select</button>
+
+ */
