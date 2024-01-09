b8960d5f04bbb49d0bd6987510b39bad98f7155e
Y1226 <yepollack@gmail.com>
2024-01-09 12:53:48 +0200

    Worked on select-react

--- src/Redux/Axios/Table/Manager/AttendanceReportAxios.jsx
+++ src/Redux/Axios/Table/Manager/AttendanceReportAxios.jsx
@@ -3,3 +3,7 @@ import axios from "axios"
 export const GetTheAttendanceForAllStudentsWithMoreDetailsBySeminarCode = (seminarCode) => {
     return (axios.get(`https://localhost:44367/api/AttendencePerCourse/GetTheAttendanceForAllStudentsWithMoreDetailsBySeminarCode/${seminarCode}`))
 }
+
+export const GetTheMaxNumberOfClassesInSeminarBySeminarCode = (seminarCode) => {
+    return (axios.get(`https://localhost:44367/api/Students/GetTheMaxNumberOfClassesInSeminarBySeminarCode/${seminarCode}`));
+}
