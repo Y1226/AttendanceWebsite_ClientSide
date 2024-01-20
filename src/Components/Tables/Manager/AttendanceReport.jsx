3c85969bfd911449b7ce2a4e814907f5fbf93a49
Michal-Hakim <gh052766@gmail.com>
2024-01-20 22:43:11 +0200

    sort the students attendance by the options from select (from react-select)

--- src/Components/Tables/Manager/AttendanceReport.jsx
+++ src/Components/Tables/Manager/AttendanceReport.jsx
@@ -5,19 +5,26 @@ import { FillAttendanceForAllStudents } from '../../../Redux/Actions/TableAction
 // import { FillAttendanceForCurrentStudent } from '../../../Redux/Actions/TableActions/Manager/PersonalAttendanceReportActions'
 import { GetTheAttendanceForAllStudentsWithMoreDetailsBySeminarCode, GetTheMaxNumberOfClassesInSeminarBySeminarCode } from '../../../Redux/Axios/Table/Manager/AttendanceReportAxios'
 import Select from 'react-select';
-import makeAnimated from 'react-select/animated';
 import { getMajorBySeminarCode } from '../../../Redux/Axios/WebSetupAxios/AddCourseToMajorAxios'
-import { LensBlur } from '@mui/icons-material'
 
-const animatedComponents = makeAnimated();
 
 export const AttendanceReport = () => {
+    
+    const compare = (a, b) => {
+        let firtsName = a.studentFirstName + " " + a.studentLastName;
+        let secondName = b.studentFirstName + " " + b.studentLastName;
+        if (firtsName === secondName) return 0;
+        return firtsName > secondName ? 1 : -1;
+    }
 
     let dispatch = useDispatch()
     let studentsAttendance = useSelector(x => x.AttendanceReportReducer.AttendanceStudentList)
+    studentsAttendance.sort((a, b) => compare(a, b))
+    const [featuredStudents, setFeaturedStudents] = useState(studentsAttendance)
     const currentUser = useSelector(x => x.SignInReducer.CurrentUser)
     const [majors, setMajors] = useState([])
     getMajorBySeminarCode(useSelector(x => x.SignInReducer.CurrentSeminarCode)).then(x => setMajors(x.data))
+    const [numberOfClasses, setNumberOfClasses] = useState(3)
     const [options, setOptions] = useState([])
 
     useEffect(() => {
@@ -28,28 +35,79 @@ export const AttendanceReport = () => {
     }, [dispatch, currentUser])
 
     useEffect(() => {
-        const derivedArray = [{value: 'מסלולים', label: 'מסלולים', isDisabled: true}, ...majors.map((item, index) => ({
+        const cycleClasses = [{ type: "grade", value: 'שנתון', label: 'שנתון', isDisabled: true }]
+        cycleClasses.push({ type: "grade", value: "A", label: "שנה א" })
+        cycleClasses.push({ type: "grade", value: "B", label: "שנה ב" })
+        cycleClasses.push({ type: "grade", value: "C", label: "שנה ג" })
+
+        const derivedArray = [{ type: "major", value: 'מסלולים', label: 'מסלולים', isDisabled: true }, ...majors.map((item, index) => ({
+            type: "major",
             value: item.majorName,
             label: item.majorName,
             key: index
         }))];
-        
-        async function fetchData() {
-            return await GetTheMaxNumberOfClassesInSeminarBySeminarCode(currentUser.seminarCode).then(x => {return x.data})
-        }
-        
-        let numberOfClasses = fetchData()
+
+        GetTheMaxNumberOfClassesInSeminarBySeminarCode(currentUser.seminarCode).then(x => { setNumberOfClasses(x.data) })
         debugger
-        let array = [numberOfClasses]
-        console.log("array: ", array);
-        // const numberClasses = [{value: 'מספר כיתה', label: 'מספר כיתה', isDisabled: true}, [...Array(numberOfClasses)].map((item, index) => ({
-        //     value: index+1,
-        //     label: index+1, 
-        //     key: index
-        // }))];
-        setOptions(derivedArray);
-        // setOptions(numberClasses)
-    }, [])
+
+        const numberClasses = [{ type: "classNumber", value: 'מספר כיתה', label: 'מספר כיתה', isDisabled: true }, ...Array(numberOfClasses).fill(0).map((item, index) => ({
+            type: "classNumber",
+            value: index + 1,
+            label: index + 1,
+            key: index
+        }))];
+
+        setOptions([...cycleClasses, ...numberClasses, ...derivedArray])
+    }, [majors, currentUser.seminarCode, numberOfClasses])
+
+    const filteringSuitableStudents = (selectedOptions) => {
+        debugger
+
+        let selectGrade = selectedOptions.filter(x => x.type === 'grade')
+        let selectClassNumber = selectedOptions.filter(x => x.type === 'classNumber')
+        let selectMajor = selectedOptions.filter(x => x.type === 'major')
+
+        const setStudent = new Set()
+        let arrayStudents = new Array()
+
+        if (selectGrade.length !== 0) {
+            for (let index = 0; index < selectGrade.length; index++) {
+                const element = selectGrade[index];
+                studentsAttendance.filter(x => { if (x.studentGrade === element.value) setStudent.add(x) })
+            }
+            arrayStudents = Array.from(setStudent)
+        }
+
+        if (setStudent.size === 0 && selectClassNumber.length !== 0) {
+            for (let index = 0; index < selectClassNumber.length; index++) {
+                const element = selectClassNumber[index];
+                studentsAttendance.filter(x => { if (x.studentClassNumber === element.value) setStudent.add(x) })
+                arrayStudents = Array.from(setStudent)
+            }
+        }
+        else if (selectClassNumber.length !== 0) {
+            for (let index = 0; index < selectClassNumber.length; index++) {
+                const element = selectClassNumber[index];
+                arrayStudents = arrayStudents.filter(x => (x.studentClassNumber === element.value))
+            }
+        }
+
+        if (setStudent.size === 0 && selectMajor.length !== 0) {
+            for (let index = 0; index < selectMajor.length; index++) {
+                const element = selectMajor[index];
+                studentsAttendance.filter(x => { if (x.firstMajorName === element.value || x.secondMajorName === element.value) setStudent.add(x) })
+                arrayStudents = Array.from(setStudent)
+            }
+        }
+        else if (selectMajor.length !== 0) {
+            for (let index = 0; index < selectMajor.length; index++) {
+                const element = selectMajor[index];
+                arrayStudents = arrayStudents.filter(x => (x.firstMajorName === element.value || x.secondMajorName === element.value))
+            }
+        }
+
+        setFeaturedStudents(arrayStudents)
+    }
 
     const openPersonalReport = (x) => {
         localStorage.setItem('currentStudentAttendance', JSON.stringify(x))
@@ -57,17 +115,16 @@ export const AttendanceReport = () => {
         window.open('/ChangingReport')
         // window.open('/PersonalAttendanceReport')
     }
-
+    
     return <div className='cardWrapper'>
         <Select
-            closeMenuOnSelect={false}
-            components={animatedComponents}
-            // defaultValue={[colourOptions[4], colourOptions[5]]}
             isMulti
+            closeMenuOnSelect={false}
             options={options}
+            onChange={filteringSuitableStudents}
         />
         {
-            studentsAttendance.map((x, y) =>
+            featuredStudents.map((x, y) =>
                 <article className="card">
                     <div key={y} className="thumb"></div>
                     <div key={`${y}0`} className="infos">
